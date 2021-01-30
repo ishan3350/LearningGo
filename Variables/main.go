9c166582be4b112996c6318f46a788d2acef5e2b
ishan <ishan@kali>
2021-01-30 18:43:32 -0500

    Another way to declare variables and variable scopes explained

--- Variables/main.go
+++ Variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var z string
+
 func main() {
 
 	// You can't use keyword for variable names. Refer to https://golang.org/ref/spec#Keywords
@@ -20,4 +22,18 @@ func main() {
 	x = 10 + 20
 	fmt.Println(x)
 
+	// anotther way to declare variables
+
+	var y = 40
+	fmt.Println(y)
+
+	// difference between short declaration and var declaration is you can't use short declaration outside of function. It will only work with in function
+
+	test()
+}
+
+func test() {
+	fmt.Println(z)
+	z = "Hello World!"
+	fmt.Println(z)
 }
