ba9bf1f1bf38aff97ce77edbd9e3656b43232658
ishan <ishan@kali>
2021-01-31 12:52:47 -0500

    Exercise 2.2

---
+++ Exercise-2.2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+
+	a := 10
+	b := 20
+
+	g := (a==b)
+	h := (a<=b)
+	i := (a>=b)
+	j := (a!=b)
+	k := (a<b)
+	l := (a>b)
+	
+	fmt.Println(g)
+	fmt.Println(h)
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
+
+}
