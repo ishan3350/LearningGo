6d01b212021bec3ce0e711bf08c1fea60132a77e
ishan <ishan@kali>
2021-01-31 12:45:04 -0500

    Exercise 2.1

---
+++ Exercise-2.1/main.go
@@ -0,0 +1,18 @@
+//Write a program that prints a number in decimal, binary and hex
+
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+
+	num := 10
+	fmt.Printf("Decimal value of num: %d ",num)
+	fmt.Printf("Binary value of num: %b ",num)
+	fmt.Printf("Hexadecimal value of num: %X ",num)
+
+
+}
+
