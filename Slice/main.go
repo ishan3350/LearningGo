3a8367354d75cd4c2585ac13e3019aba875ea192
ishan <ishan@kali>
2021-02-01 06:43:38 -0500

    looping through slice using range

--- Slice/main.go
+++ Slice/main.go
@@ -12,7 +12,7 @@ func simpleslice(){
 
 	// it is good practice to use slice instead of arrays
 
-	// creation of slice. Slice allows to group together values of the same type
+	// creation of slice using composite literal. Slice allows to group together values of the same type
 	x := []int{1,2,3,4,5}
 	fmt.Println(x)
 
@@ -20,9 +20,9 @@ func simpleslice(){
 	strs := []string{"test1","test2","test3","test4","test5"}
 	fmt.Println(strs)
 
+	// looping through slice using range
 
-	
-
-
-
+	for key,value := range x{
+		fmt.Println(key,value)
+	}
 }
