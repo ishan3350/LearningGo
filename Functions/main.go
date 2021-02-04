3c3d9854e1baccfce6d1b19d80bf5e2c3d65afd1
repl.it user <replituser@example.com>
2021-02-04 15:36:56 +0000

    Simple function

---
+++ Functions/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+  "fmt"
+)
+
+func main(){
+  // fmt.Println("Hello World!")
+
+  sayhello()
+}
+
+func sayhello(){
+  fmt.Println("Hello from function")
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.14
+
+require (
+	github.com/Pallinder/go-randomdata v1.2.0
+	rsc.io/sampler v1.99.99
+)
