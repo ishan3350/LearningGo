104ab76b2cda7b2e1dd64115221b4fe484de927d
ishan <ishan@kali>
2021-02-05 17:42:55 -0500

    Function with parameter or argument

--- Functions/main.go
+++ Functions/main.go
@@ -8,8 +8,20 @@ func main(){
   // fmt.Println("Hello World!")
 
   sayhello()
+
+  displaymessage("Hello World!")
+
+  //We can call function as many times as we want
+
+  displaymessage("Hello again!")
 }
 
 func sayhello(){
   fmt.Println("Hello from function")
+}
+
+// function with parameter
+
+func displaymessage(msg string){
+  fmt.Println(msg)
 }
