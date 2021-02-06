b04d4a99fa2b370b0c093a9e098adb5c41525104
ishan <ishan@kali>
2021-02-05 20:43:08 -0500

    Changed directory name to Functions-AND-Interface

--- Functions-AND-Interface/simpleInterface.go
+++ Functions-AND-Interface/simpleInterface.go
@@ -19,6 +19,13 @@ type advancedprogrammer struct{
 	experience int
 }
 
+type expertprogrammer struct{
+	name string
+	age int
+	experience int
+	numawards int
+}
+
 func(p juniorprogrammer) code(){
 	fmt.Println("Name: ",p.name)
 	fmt.Println("Age: ",p.age)
@@ -31,6 +38,16 @@ func (p advancedprogrammer)code(){
 
 
 }
+
+func (p expertprogrammer)code(){
+	fmt.Println("Name:", p.name)
+	fmt.Println("Age: ",p.age)
+	fmt.Println("Experience: ",p.experience)
+	fmt.Println("Number of awards: ",p.numawards)
+
+}
+
+
 func main(){
 
 	p1 := juniorprogrammer{
@@ -44,7 +61,15 @@ func main(){
 		experience: 10,
 	}
 
+	p3 := expertprogrammer{
+		name: "Programmer 3",
+		age: 25,
+		experience: 21,
+		numawards: 5,
+	}
+
 	p1.code()
 	p2.code()
+	p3.code()
 }
 
