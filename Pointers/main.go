12045767154c90351c11e8dc02e03ca608de0e6a
ishan <ishan@kali>
2021-02-06 15:32:23 -0500

    Simple pointer program

--- Pointers/main.go
+++ Pointers/main.go
@@ -11,4 +11,22 @@ func main(){
 	// getting memory address of variable
 	fmt.Println(&a)
 
+	// getting type (*int)
+	fmt.Printf("%T",&a)
+
+	// & person will give you adderess of variable in memory where it is stored
+
+	var b *int = &a
+	fmt.Println(b)
+
+	//accessing pointer value
+	fmt.Println(*b)
+
+	// *& will also give value at that address
+	fmt.Println(*&a)
+
+	// changing value using pointer
+	*b = 20
+	fmt.Println(a)
+
 }
