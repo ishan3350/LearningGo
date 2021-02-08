b0a0c8b9f10022873325f4185fd838288799b5da
ishan <ishan@kali>
2021-02-07 20:12:01 -0500

    bcrypt hash

---
+++ Application/bcrypt.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main(){
+	s := "Password123!"
+	hash,_ := bcrypt.GenerateFromPassword([]byte(s),10)
+	fmt.Println(string(hash))
+
+	// checking if plaintext is matching with hash or not (Password validation method)
+	check := bcrypt.CompareHashAndPassword(hash,[]byte(s))
+
+	if(check != nil){
+		fmt.Println("Invalid password")
+	}else{
+		fmt.Println("Success")
+	}
+}
--- Application/custom-sort.go
+++ Application/custom-sort.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"sort"
 )
+
 // custom sort
-type Person struct{
+type Person struct {
 	Name string
-	Age int
+	Age  int
 }
 type ByAge []Person
 
@@ -15,32 +16,30 @@ func (a ByAge) Len() int           { return len(a) }
 func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
 
-func main(){
+func main() {
 
 	p1 := Person{
-		Name:"charlie",
-		Age:20,
+		Name: "charlie",
+		Age:  20,
 	}
 
 	p2 := Person{
 		Name: "bravo",
-		Age: 10,
+		Age:  10,
 	}
 
 	p3 := Person{
 		Name: "test",
-		Age: 15,
+		Age:  15,
 	}
 
 	// combining all struct object in one slice
 
-	people := []Person{p1,p2,p3}
-
+	people := []Person{p1, p2, p3}
 
 	// sorting  data in slice by Age
 	sort.Sort(ByAge(people))
 
 	fmt.Println(people)
 
-	
 }
