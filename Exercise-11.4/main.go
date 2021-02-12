e7a0467957d4141d0de63a3211fdcc5c4c248360
ishan <ishan@kali>
2021-02-12 13:30:59 -0500

    Exercise 11.4

---
+++ Exercise-11.4/main.go
@@ -0,0 +1,38 @@
+package main
+
+// returning struct object as an error
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		// write your error code here
+		return 0, sqrtError{
+			lat:  "test lat",
+			long: "test long",
+			err:  errors.New("More coffee needed"),
+		}
+	}
+	return 42, nil
+}
